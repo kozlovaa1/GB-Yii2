f33a1ec58b9723ca3a53e0f822561c619dc457aa
ak <a.kozlov@studioratio.ru>
2018-10-25 10:00:49 +0500

    Добавил свойства события, обновил настройки composer, редактировал меню

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -41,7 +41,7 @@ AppAsset::register($this);
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
-            ['label' => 'События', 'url' => ['/activity/index']],
+            ['label' => 'События', 'url' => ['/activity/index'], 'class' => 'btn btn-success'],
             ['label' => 'Hello', 'url' => ['/site/hello']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
